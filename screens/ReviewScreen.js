7676f1e86c8b5dff91ff38175157b399b1084e02
Volodymyr Lipunov <volo_lipu@yahoo.com>
2017-05-30 10:21:57 -0700

    first commit

---
+++ screens/ReviewScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class extends Component {
+  render() {
+    return <View>
+      <Text>Review Screen</Text>
+      <Text>Review Screen</Text>
+      <Text>Review Screen</Text>
+      <Text>Review Screen</Text>
+      <Text>Review Screen</Text>
+    </View>;
+  }
+}
